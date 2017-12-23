ac91e87969709fa8cc8e468c0f5aba9997b2476c
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2017-12-24 00:47:00 +0100

    Refactor day 7

--- day7.py
+++ day7.py
@@ -27,38 +27,33 @@ def part1(graph):
 
 
 def part2(graph):
-    root = part1(graph)
-
-    def children(node):
+    def _children(node):
         return [(edge[1], graph.nodes[edge[1]]["weight"]) for edge in graph.out_edges(node)]
 
-    def calculate_weights(node):
-        def _weights(node, return_tuples=True):
-            if return_tuples:
-                return [(child[0], sum(_weights(child[0], return_tuples=False)) + child[1]) for child in children(node)]
-            return [sum(_weights(child[0], return_tuples=False)) + child[1] for child in children(node)]
+    def _weights(node, return_tuples=True):
+        if return_tuples:
+            return [(child[0], sum(_weights(child[0], return_tuples=False)) + child[1]) for child in _children(node)]
+        return [sum(_weights(child[0], return_tuples=False)) + child[1] for child in _children(node)]
 
-        current = node
-        while True:
-            weights = pd.DataFrame(_weights(current))
-            unique = weights.iloc[:, 1].value_counts()
-            if len(unique) == 2:
-                # observe the unbalanced child node
-                majority_weight = weights.iloc[:, 1].value_counts().idxmax()
-                unbalanced_node = weights.loc[weights.iloc[:, 1] != majority_weight, 0]
-                unbalanced_child_weights = _weights(unbalanced_node, return_tuples=False)
+    current = part1(graph)
+    while True:
+        weights = pd.DataFrame(_weights(current))
+        unique = weights.iloc[:, 1].value_counts()
+        if len(unique) == 2:
+            # observe the unbalanced child node
+            majority_weight = weights.iloc[:, 1].value_counts().idxmax()
+            unbalanced_node = weights.loc[weights.iloc[:, 1] != majority_weight, 0]
+            unbalanced_child_weights = _weights(unbalanced_node, return_tuples=False)
 
-                if unbalanced_child_weights[1:] == unbalanced_child_weights[:-1]:
-                    # if the children are balanced, the root node is unbalanced
-                    return majority_weight - sum(unbalanced_child_weights)
-                else:
-                    # observe the next unbalanced node
-                    current = unbalanced_node
+            if unbalanced_child_weights[1:] == unbalanced_child_weights[:-1]:
+                # if the children are balanced, the root node is unbalanced
+                return majority_weight - sum(unbalanced_child_weights)
             else:
-                assert len(unique) == 1
-                return 0
-
-    return calculate_weights(root)
+                # observe the next unbalanced node
+                current = unbalanced_node
+        else:
+            assert len(unique) == 1
+            return 0
 
 
 print(part1(read_graph('assets/day7_demo.txt')))
