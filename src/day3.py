1a0b03fb65bba47455354cff5957d17eb2af3442
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2018-04-01 22:39:37 +0200

    Better solution for day 3

--- src/day3.py
+++ src/day3.py
@@ -2,25 +2,35 @@
 # (c) blu3r4y
 
 
-def mod_offset(n):
-    return 1 if (n % 8 == 0) else 0
+import math
 
 
-def mod_len(n):
-    return 1 + (n // 8) + mod_offset(n - 6)
+def clamp(minvalue, maxvalue, value):
+    return max(minvalue, min(value, maxvalue))
 
 
-def mod_sums(n):
-    result, i, j = 1, 0, 0
-    while i < n:
-        result += min(mod_len(j), n - i) * (-1 if j % 2 else 1)
-        i += mod_len(j)
-        j += 1
-    return result
+def coordinate(n):
+    if n == 0:
+        return (0, 0)
+
+    # base block and offset rest
+    base = math.floor((math.sqrt(4 * n - 3) - 5) / 4) + 1
+    rest = n - (6 * base + 8 * ((base - 1) * base) // 2)
+
+    # number of steps (!) on the right/up and left/down sides in this block
+    rightUp = 2 * base + 1
+    leftDown = rightUp + 1
+
+    right = clamp(0, rest, rightUp)
+    up = clamp(0, rest - rightUp, rightUp)
+    left = clamp(0, rest - rightUp - rightUp, leftDown)
+    down = clamp(0, rest - rightUp - rightUp - leftDown, leftDown)
+
+    return (-base + right - left, base + down - up)
 
 
 def part1(n):
-    return (n - 1) if (n < 4) else mod_sums(n - 2)
+    return sum([abs(e) for e in coordinate(n - 1)])
 
 
 def part2():
@@ -37,13 +47,6 @@ def part2():
 
     return 312453
 
-
-# test with https://oeis.org/A214526
-test = [0, 1, 2, 1, 2, 1, 2, 1, 2, 3, 2, 3, 4, 3, 2, 3, 4, 3, 2, 3, 4, 3, 2, 3, 4, 5, 4, 3, 4, 5, 6, 5, 4, 3, 4, 5, 6,
-        5, 4, 3, 4, 5, 6, 5, 4, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 7, 6, 5, 4, 5, 6, 7, 8, 7, 6, 5, 4, 5, 6, 7, 8, 7,
-        6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 6, 7, 8, 9,
-        10, 9, 8, 7, 6, 5, 6, 7, 8, 9, 10]
-print([part1(i) - test[i - 1] for i in range(1, len(test))])
 
 # solve
 print(part1(312051))
